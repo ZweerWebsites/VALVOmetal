64c2c7073d2af81c8c8401f5516a5b851c1ca669
Zweer <niccolo@olivieriachille.com>
2017-08-23 00:02:52 +0200

    Added the "Theme Options" page

--- wp-content/themes/valvometal/inc/admin.php
+++ wp-content/themes/valvometal/inc/admin.php
@@ -3,3 +3,9 @@
 add_action('admin_menu', function () {
     remove_menu_page('edit.php');
 });
+
+add_action('init', function () {
+    if(function_exists('acf_add_options_page')) {
+        acf_add_options_page('Opzioni Tema');
+    }
+});
