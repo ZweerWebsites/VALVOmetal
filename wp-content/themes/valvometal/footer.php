9651c3566c744a7f6918401e4077e787b3ad4caf
Zweer <niccolo@olivieriachille.com>
2017-09-05 20:21:17 +0200

    Added the footer

--- wp-content/themes/valvometal/footer.php
+++ wp-content/themes/valvometal/footer.php
@@ -1,3 +1,14 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                Sitindustrie VALVOmetal - Ital Parts Group Srl  |. Vat Nr. 11997650152. |. Via Orlonghetto, 3 - 13018 Valduggia (VC) - Italy
+            </div>
+        </div>
+    </div>
+</footer>
+
+
 <?php
 
 get_footer('html');
