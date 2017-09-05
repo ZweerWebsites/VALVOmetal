5da18e1395ead05419a222c38f54dc5c495ef816
Zweer <niccolo@olivieriachille.com>
2017-09-05 20:46:47 +0200

    Added the customer map part

--- wp-content/themes/valvometal/templates/parts/customer-map.php
+++ wp-content/themes/valvometal/templates/parts/customer-map.php
@@ -1,2 +1,24 @@
 <?php
 
+$stats = get_field('stats', 'option');
+
+?>
+
+<div class="customer_map-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <div class="card-group">
+                    <?php foreach ($stats as $stat) : ?>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-title"><?= $stat['value'] ?></div>
+                            <div class="card-text"><?= $stat['text'] ?></div>
+                        </div>
+                    </div>
+                    <?php endforeach ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
