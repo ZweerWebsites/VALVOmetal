0f73afeb1ed3b94f3fc021a9eed07feaf3b40eb3
Zweer <niccolo@olivieriachille.com>
2017-09-05 20:16:47 +0200

    Deleted default themes

--- wp-content/themes/valvometal/front-page.php
+++ wp-content/themes/valvometal/front-page.php
@@ -6,5 +6,6 @@ get_template_part('templates/parts/production');
 get_template_part('templates/parts/quality');
 get_template_part('templates/parts/news');
 get_template_part('templates/parts/newsletter');
+get_template_part('templates/parts/customer', 'map');
 
 get_footer();
