4a0b0e81a61d9d156f41056869abc1adb36d0588
Zweer <niccolo@olivieriachille.com>
2017-09-05 23:56:19 +0200

    Added the double header

--- wp-content/themes/valvometal/_js/main.js
+++ wp-content/themes/valvometal/_js/main.js
@@ -1,3 +1,18 @@
 import 'bootstrap';
+import $ from 'jquery';
 
-console.log('ciaoaoaoaoaoaoaoa');
+$(() => {
+  const threshold = 400;
+  const class2add = 'navbar-hidden-visible';
+
+  const $navbar = $('.navbar-hidden');
+  const $window = $(window);
+
+  $window.scroll(() => {
+    if ($window.scrollTop() > threshold) {
+      $navbar.addClass(class2add);
+    } else {
+      $navbar.removeClass(class2add);
+    }
+  });
+});
--- wp-content/themes/valvometal/header.php
+++ wp-content/themes/valvometal/header.php
@@ -5,12 +5,32 @@ require_once('inc/vendor/class-wp-bootstrap-navwalker.php');
 get_header('html');
 
 $logoId = get_field('logo', 'option');
+$logoDarkId = get_field('logo_dark', 'option');
 
 ?>
 
-<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-transparent">
+<nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
     <a class="navbar-brand" href="<?php get_permalink(get_page_by_path('homepage')) ?>">
         <?php the_retina_image($logoId) ?>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <?php wp_nav_menu([
+        'menu' => 'main-menu',
+        'container' => 'div',
+        'container_id' => 'navbarNavAltMarkup',
+        'container_class' => 'collapse navbar-collapse align-self-start',
+        'menu_class' => 'navbar-nav ml-auto',
+        'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+        'walker' => new WP_Bootstrap_Navwalker(),
+    ]) ?>
+</nav>
+
+<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-white navbar-hidden">
+    <a class="navbar-brand" href="<?php get_permalink(get_page_by_path('homepage')) ?>">
+        <?php the_retina_image($logoDarkId) ?>
     </a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
